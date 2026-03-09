183086e49ed8a81859856604409fe14c08873a96
Galen <1453811292@qq.com>
2026-03-09 16:23:01 +0800

    feat: font

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,13 @@
 import "./globals.css";
+import { Geist } from 'next/font/google'
+
+const geist = Geist({ subsets: ['latin'] })
+
+
 
 export default function RootLayout({children}: Readonly<{children: React.ReactNode}>) {
   return (
-    <html lang="en">
+    <html lang="en" className={geist.className}>
       <body className="antialiased font-sans">
         {children}
       </body>
