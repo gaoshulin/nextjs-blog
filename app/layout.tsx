25a4573ca92e2a323eb2bb8796d79312b161c358
Galen <1453811292@qq.com>
2026-03-09 16:54:23 +0800

    feat: add metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,13 @@
 import "./globals.css";
 import { Geist } from 'next/font/google'
+import type { Metadata } from "next";
 
 const geist = Geist({ subsets: ['latin'] })
 
+export const metadata: Metadata = {
+  title: "Next.js Blog",
+  description: "A blog about Next.js",
+}
 
 
 export default function RootLayout({children}: Readonly<{children: React.ReactNode}>) {
